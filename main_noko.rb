12071bdd9c64d1053edf2ac1e9471ec8d2e1c5c4
anklapashchuk20 <samsunggs048@gmail.com>
2021-11-26 00:05:52 +0200

    Initial commit

--- main_noko.rb
+++ main_noko.rb
@@ -16,18 +16,18 @@ books = doc.css('.listicle-slide').map do |book|
         title: book.css('.listicle-slide-hed-text').text,
         author: book.css('.product-slide-brand').text,
         vendor: book.css('.product-slide-vendor').text,
-        price: book.css('.list-price').text.to_f,
-        sale_price: book.css('.discount-price').text.to_f,
+        price: book.css('.list-price').text,
         description: book.css('.slideshow-slide-dek').text
     }
 end
 
 #puts books
 
-head = []
-head = ["Rating", "Title", "Author", "Vendor", "Price", "Sale price", "Description"]
+#table_headers = []
+#table_headers = ["Rating","Title","Author","Vendor","Price","Description"]
+
 
 CSV.open('Books.csv', 'w') do |csv|
-    csv << head
+
     books.each { |b| csv << b.values }
 end
